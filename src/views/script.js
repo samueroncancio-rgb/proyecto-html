7fb08f4394560b15d50a75b1e08fd62e0c64d014
samuel <samueroncancio@gmail.com>
2026-04-21 12:23:49 -0500

    hice todo nuevamente

---
+++ src/views/script.js
@@ -0,0 +1,30 @@
+const menu = document.querySelector(".menu");
+const links = document.querySelector(".links");
+const link = document.querySelectorAll(".link");
+const overlay = document.querySelector("#overlay");
+
+
+
+
+menu.addEventListener("click",()=>{
+    links.classList.toggle("visible");
+    overlay.classList.toggle("overlay")
+    document.body.classList.toggle("no-scroll")
+})
+
+overlay.addEventListener("click",()=>{
+    links.classList.remove("visible");
+    overlay.classList.remove("overlay")
+    document.body.classList.remove("no-scroll")
+})
+
+
+link.forEach(a => {
+    a.addEventListener("click",()=>{
+        setTimeout(()=>{
+            links.classList.remove("visible");
+            overlay.classList.remove("overlay")
+            document.body.classList.remove("no-scroll")
+        },100)
+    })
+});
